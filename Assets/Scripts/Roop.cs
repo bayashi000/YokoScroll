bda0ed23ed78bcc30485cd55fd9d680307ce36c7
bayashi000 <bayashiko1020@yahoo.co.jp>
2019-02-13 16:12:42 +0900

    ループ処理

---
+++ Assets/Scripts/Roop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //当たり判定
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            var roop = GameObject.FindWithTag("RoopPoint");
+            other.gameObject.transform.position = roop.transform.position;
+
+            Debug.Log("リスタート");
+        }
+
+    }
+}
---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //当たり判定
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            var respawn = GameObject.FindWithTag("Respawn");
+            other.gameObject.transform.position = respawn.transform.position;
+
+            Debug.Log("リスタート");
+        }
+
+    }
+}
